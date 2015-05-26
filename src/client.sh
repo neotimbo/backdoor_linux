674b19af2cdf04556429b22dcc82b9d9d3fbd5eb
opk1 <dsathanielle@gmail.com>
2015-05-26 04:25:28 -0700

    minor changes

--- src/client.sh
+++ src/client.sh
@@ -9,7 +9,7 @@ fi
 
 PORT=$1
 IP=$2
-TRY=2
+TRY=1
 
 n=$RANDOM
 A=$(((RANDOM%899+100)*100+82))
@@ -17,9 +17,10 @@ B=$(((RANDOM%899+100)*100+71))
 C=$(((RANDOM%899+100)*100+66))
 
 
-nc -l $PORT &
+nc -l -p $PORT &
 
-hping3 -2 -c 1 -N $A -d 111 -s 53 -p $PORT $IP
-hping3 -2 -c 1 -N $B -d 111 -s 53 -p $PORT $IP
-hping3 -2 -c 1 -N $C -d 111 -s 53 -p $PORT $IP
+hping3 -2 -c $TRY -N $A -d 111 -s 53 -p $PORT $IP
+hping3 -2 -c $TRY -N $B -d 111 -s 53 -p $PORT $IP
+hping3 -2 -c $TRY -N $C -d 111 -s 53 -p $PORT $IP
 
+fg
